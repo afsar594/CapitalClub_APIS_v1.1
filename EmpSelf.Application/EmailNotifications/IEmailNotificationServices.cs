44efd6ea426f72a37b04bb2da840dad6b490fbd8
Irfan Ud Din <irfanmiankhan@gmail.com>
2025-11-26 14:29:38 +0500

    save

--- EmpSelf.Application/EmailNotifications/IEmailNotificationServices.cs
+++ EmpSelf.Application/EmailNotifications/IEmailNotificationServices.cs
@@ -10,13 +10,10 @@ namespace EmpSelf.Application.EmailNotifications
 {
     public interface IEmailNotificationServices
     {
-        public interface IEmailNotificationServices
-        {
-            Task<bool> SendMultipleTemplateEmailsToMultipleRecipientsAsync(List<EmailAddress> toEmails, string templateId, List<object> dynamicTemplateData, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
-            Task<bool> SendSingleTemplateEmailToMultipleRecipientsAsync(List<EmailAddress> toEmails, string templateId, object dynamicTemplateData, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
+        Task<bool> SendMultipleTemplateEmailsToMultipleRecipientsAsync(List<EmailAddress> toEmails, string templateId, List<object> dynamicTemplateData, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
+        Task<bool> SendSingleTemplateEmailToMultipleRecipientsAsync(List<EmailAddress> toEmails, string templateId, object dynamicTemplateData, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
 
-            Task<bool> SendMultipleEmailToMultipleRecipientsAsync(List<EmailAddress> toEmails, List<string> subject, string message, string htmlContent, List<Dictionary<string, string>> substitutions, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
-            Task<bool> SendSingleEmailToMultipleRecipientsAsync(List<EmailAddress> toEmails, string subject, string message, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
-        }
+        Task<bool> SendMultipleEmailToMultipleRecipientsAsync(List<EmailAddress> toEmails, List<string> subject, string message, string htmlContent, List<Dictionary<string, string>> substitutions, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
+        Task<bool> SendSingleEmailToMultipleRecipientsAsync(List<EmailAddress> toEmails, string subject, string message, List<EmailAddress> bc = null, List<EmailAddress> cc = null);
     }
 }
